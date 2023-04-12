4f68b95fee951627ed3f2900a22c7aece0847c52
JensC <jenscollaertprive@hotmail.com>
2023-04-12 18:17:44 +0200

    10 min cache

--- routes/api.js
+++ routes/api.js
@@ -7,17 +7,17 @@ const request = require('../request')
 let cache = apicache.middleware
 
 // Gamertag endpoint
-router.get('/v1/gamertag/:gamertag', cache('2 minutes'), async (req, res) => {
+router.get('/v1/gamertag/:gamertag', cache('10 minutes'), async (req, res) => {
     request.requestAPIHandler(request.BASE_API_URL + request.FRIENDS + req.params.gamertag, res)
 })
 
 // Xuid endpoint
-router.get('/v1/xuid/:xuid', cache('2 minutes'), async (req, res) => {
+router.get('/v1/xuid/:xuid', cache('10 minutes'), async (req, res) => {
     request.requestAPIHandler(request.BASE_API_URL + request.ACCOUNT + req.params.xuid, res)
 })
 
 // Fuuid endpoint
-router.get('/v1/fuid/:fuuid', cache('2 minutes'), async (req, res) => {
+router.get('/v1/fuid/:fuuid', cache('10 minutes'), async (req, res) => {
     request.requestAPIHandler(request.BASE_API_URL + request.ACCOUNT + converters.makeXuid(req.params.fuuid), res)
 })
 
