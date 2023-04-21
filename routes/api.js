5d984ad1b0edbb3cc900310af7423d876fd361b0
JensC <jenscollaertprive@hotmail.com>
2023-04-21 23:20:19 +0200

    Add Java api

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ import express from 'express'
 import apicache from 'apicache'
 import profile from '../js/profileSetup.js'
 import xboxRequest from '../js/xboxRequest.js'
+import minecraftRequest from '../js/minecraftRequest.js'
 
 const router = express.Router()
 const gamertagApiPath = '/users/gt('
@@ -11,9 +12,17 @@ const errorMessage = 'Could not get account details from xbox api.'
 
 const cacheMiddleware = apicache.middleware(cacheTime)
 
-const respond = async (xboxResponse, res) => {
+const bedrockRespond = async (xboxResponse, res) => {
   try {
     const profileData = await profile.bedrockSetup(xboxResponse.body)
+    res.status(200).send(profileData)
+  } catch (error) {
+    res.status(400).json({ message: errorMessage })
+  }
+}
+const javaRespond = async (javaResponse, res) => {
+  try {
+    const profileData = await profile.javaSetup(javaResponse)
     res.status(200).send(profileData)
   } catch (error) {
     res.status(400).json({ message: errorMessage })
@@ -23,7 +32,7 @@ const respond = async (xboxResponse, res) => {
 router.get('/v1/bedrock/gamertag/:gamertag', cacheMiddleware, async (req, res) => {
   try {
     const xboxResponse = await xboxRequest.requestXBLData(`${gamertagApiPath}${req.params.gamertag})/profile/settings`)
-    respond(xboxResponse, res)
+    bedrockRespond(xboxResponse, res)
   } catch (error) {
     res.status(404).json({ message: 'User not found.' })
   }
@@ -32,7 +41,7 @@ router.get('/v1/bedrock/gamertag/:gamertag', cacheMiddleware, async (req, res) =
 router.get('/v1/bedrock/xuid/:xuid', cacheMiddleware, async (req, res) => {
   try {
     const xboxResponse = await xboxRequest.requestXBLData(`${xuidApiPath}${req.params.xuid})/profile/settings`)
-    respond(xboxResponse, res)
+    bedrockRespond(xboxResponse, res)
   } catch (error) {
     res.status(404).json({ message: 'User not found.' })
   }
@@ -42,7 +51,25 @@ router.get('/v1/bedrock/fuid/:fuuid', cacheMiddleware, async (req, res) => {
   try {
     const xuid = profile.createXuid(req.params.fuuid)
     const xboxResponse = await xboxRequest.requestXBLData(`${xuidApiPath}${xuid})/profile/settings`)
-    respond(xboxResponse, res)
+    bedrockRespond(xboxResponse, res)
+  } catch (error) {
+    res.status(404).json({ message: 'User not found.' })
+  }
+})
+
+router.get('/v1/java/username/:username', cacheMiddleware, async (req, res) => {
+  try {
+    const minecraftResponse = await minecraftRequest.requestMCData(req.params.username, true)
+    javaRespond(minecraftResponse, res)
+  } catch (error) {
+    res.status(404).json({ message: 'User not found.' })
+  }
+})
+
+router.get('/v1/java/uuid/:uuid', cacheMiddleware, async (req, res) => {
+  try {
+    const minecraftResponse = await minecraftRequest.requestMCData(req.params.uuid, false)
+    javaRespond(minecraftResponse, res)
   } catch (error) {
     res.status(404).json({ message: 'User not found.' })
   }
